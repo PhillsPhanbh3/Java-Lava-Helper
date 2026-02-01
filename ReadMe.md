52a6b18bdb53afeed77416fd1704f20666fd3e5e
PhillsPhanbh3 <bkohut131@gmail.com>
2026-02-01 14:18:02 -0500

    Add environment configuration and update database connection handling

---
+++ ReadMe.md
@@ -0,0 +1,13 @@
+# Java Lava Helper Public Archive - Version 1
+
+This is just a base system for you to build on with a helper bot for your discord server!
+
+## What packages will you need to install?
+
+Discord.js
+dotenv
+mongoose
+
+# Can I contribute to this repo publicly?
+
+No you can't as it is a archived repo.
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { Client, PermissionsBitField, Events } = require('discord.js');
+require("dotenv/config");
+const { Client, PermissionsBitField } = require('discord.js');
 const mongoose = require('mongoose');
 const LogError = require('./utils/LogError.js');
 const client = new Client({ intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'DirectMessages']});
-client.config = require('./config.json');
 client.cooldowns = new Map();
 client.cache = new Map();
 require('./utils/ComponentLoader.js')(client);
@@ -10,12 +10,12 @@ require('./utils/EventLoader.js')(client);
 require('./utils/RegisterCommands.js')(client);
 
 ( async function() {
-	if (!client.config.mongoURL) return console.warn('MongoDB URL is not provided in the config.json file, skipping database connection...');
-	await mongoose.connect(client.config.mongoURL);
+	if (!process.env.mongoURL) return console.warn('MongoDB URL is not provided in the environment variables, skipping database connection...');
+	await mongoose.connect(process.env.mongoURL);
 })();
 
 console.log(`Logging in...`);
-client.login(client.config.TOKEN);
+client.login(process.env.TOKEN);
 client.on('clientReady', function () {
     console.log(`Logged in as ${client.user.tag}!`);
 
@@ -29,14 +29,14 @@ async function InteractionHandler(interaction, type) {
     const component = client[type].get(interaction.customId ?? interaction.commandName);
     if (!component) return;
     const devs = [
-        '1163939796767473698'
+        process.env.OWNER_ID
     ];
     try {
         console.log(`[INTERACTION] ${interaction.user.tag} in ${interaction.guild ? interaction.guild.name : 'DMs'} triggered ${type} ${interaction.customId ?? interaction.commandName}`);
         if (component.admin) {
             if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) return await interaction.reply({ content: `⚠️ Only administrators can use this command!`, flags: 64 });
         } if (component.owner) {
-            if (interaction.user.id !== '1163939796767473698') return await interaction.reply({ content: `⚠️ Only bot owners can use this command!`, flags: 64 });
+            if (interaction.user.id !== process.env.OWNER_ID) return await interaction.reply({ content: `⚠️ Only bot owners can use this command!`, flags: 64 });
         }
         await component.execute(interaction, client);
     } catch (error) {
